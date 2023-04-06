6ef887c153e4208b293fb4e47cd04cd9f460002a
Nikolinc <tsigulskynikita@gmail.com>
2023-04-06 15:24:45 +0500

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,17 @@ import "./index.css";
 import {
   BrowserRouter,
   createBrowserRouter,
+  HashRouter,
   Route,
   RouterProvider,
   Routes,
 } from "react-router-dom";
 
-
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/:id" element={<App />} />
+        <Route path="tx/:id" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
